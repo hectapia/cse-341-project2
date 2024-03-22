feed4b7529c2711894db6cc8d2fa74016d6aeb64
hectapia <oli21007@byui.edu>
2024-03-21 23:41:30 -0600

    Adding security measures using OAuth

--- swagger.js
+++ swagger.js
@@ -2,8 +2,8 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
     info: {
-        title: 'Users API',
-        description: 'An API for Users API'
+        title: 'Univeristy API',
+        description: 'An API for Univerity API'
     },
     host: 'localhost:3001',
     Schemes: ['http', 'https']
